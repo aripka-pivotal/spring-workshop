109b7b58429aa9fcd166858c99bab8afa7043d1b
aripka <aripka@10.30.90.52>
2017-06-24 13:57:22 -0500

    less specific collection type

--- complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRepo.java
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRepo.java
@@ -1,14 +1,12 @@
 package io.spring.workshop.patientsvc;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface PatientRepo extends CrudRepository<Patient, Long> {
 	
-	public List<Patient> findByName(@Param("name") String name);
+	public Iterable<Patient> findByName(@Param("name") String name);
 	
-	public List<Patient> findByNameStartsWith(@Param("name") String name);
+	public Iterable<Patient> findByNameStartsWith(@Param("name") String name);
 
 }
