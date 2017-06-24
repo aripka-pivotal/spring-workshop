88e7184a394b27a0e6b5a1063f23f9d389e8dd1e
aripka <aripka@10.30.90.52>
2017-06-24 13:38:34 -0500

    add find by name starts with

--- complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRepo.java
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRepo.java
@@ -8,5 +8,7 @@ import org.springframework.data.repository.query.Param;
 public interface PatientRepo extends CrudRepository<Patient, Long> {
 	
 	public List<Patient> findByName(@Param("name") String name);
+	
+	public List<Patient> findByNameStartsWith(@Param("name") String name);
 
 }
