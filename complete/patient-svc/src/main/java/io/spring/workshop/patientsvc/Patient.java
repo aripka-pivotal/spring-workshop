b7e9432759f6e9442b881b26bf3daaab4ec2cb13
aripka <aripka@10.30.90.52>
2017-06-24 13:02:40 -0500

    add patient entity

---
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/Patient.java
@@ -0,0 +1,49 @@
+package io.spring.workshop.patientsvc;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Patient {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dateofBirth;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateofBirth() {
+		return dateofBirth;
+	}
+
+	public void setDateofBirth(Date dateofBirth) {
+		this.dateofBirth = dateofBirth;
+	}
+	
+	
+}
