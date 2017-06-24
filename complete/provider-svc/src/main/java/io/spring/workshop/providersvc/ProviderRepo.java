4e96b397fddd00d6de684338cb5f561a5b9f5066
aripka <aripka@10.30.90.52>
2017-06-24 14:06:54 -0500

    override the deatuls paths and collection references

--- complete/provider-svc/src/main/java/io/spring/workshop/providersvc/ProviderRepo.java
+++ complete/provider-svc/src/main/java/io/spring/workshop/providersvc/ProviderRepo.java
@@ -2,7 +2,9 @@ package io.spring.workshop.providersvc;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="provider", collectionResourceRel="providerCollection")
 public interface ProviderRepo extends CrudRepository<Provider, Long> {
 
 	public Iterable<Provider> findByEmail(@Param("email") String email);
