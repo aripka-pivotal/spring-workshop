8441e22eeaad75593964eba440092c2cc55d1102
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-06-24 12:51:37 -0500

    patient-svc start

---
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientSvcApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.patientsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSvcApplication.class, args);
+	}
+}
