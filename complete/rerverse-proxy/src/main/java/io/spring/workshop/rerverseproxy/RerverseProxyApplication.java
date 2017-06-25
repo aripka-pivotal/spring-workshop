a9477359a28a29cb72e02d8650b21af59255bf33
aripka <aripka@10.80.4.138>
2017-06-25 14:16:03 -0500

    basic working version

--- complete/rerverse-proxy/src/main/java/io/spring/workshop/rerverseproxy/RerverseProxyApplication.java
+++ complete/rerverse-proxy/src/main/java/io/spring/workshop/rerverseproxy/RerverseProxyApplication.java
@@ -2,11 +2,19 @@ package io.spring.workshop.rerverseproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class RerverseProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RerverseProxyApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }
