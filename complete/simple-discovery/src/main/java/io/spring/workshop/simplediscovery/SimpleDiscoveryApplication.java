21b958557bfa7b18b965c4a94e7c76c415531a28
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-06-25 13:06:00 -0500

    simple-discovery start

---
+++ complete/simple-discovery/src/main/java/io/spring/workshop/simplediscovery/SimpleDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.simplediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDiscoveryApplication.class, args);
+	}
+}
