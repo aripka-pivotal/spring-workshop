1d9516ddf75a66bbff1750a4cff6d95e7f234da5
aripka <aripka@10.80.4.138>
2017-06-25 13:01:43 -0500

    enable service discovery

--- complete/message-svc/src/main/java/io/spring/workshop/messagesvc/MessageSvcApplication.java
+++ complete/message-svc/src/main/java/io/spring/workshop/messagesvc/MessageSvcApplication.java
@@ -2,8 +2,10 @@ package io.spring.workshop.messagesvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MessageSvcApplication {
 
 	public static void main(String[] args) {
