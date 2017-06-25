f085436e9cc7d792cbd601980ea3e23055e863c1
aripka <aripka@10.80.4.138>
2017-06-25 13:48:43 -0500

    finish circuit-db

--- complete/circuit-db/src/main/java/io/spring/workshop/circuitdb/CircuitDbApplication.java
+++ complete/circuit-db/src/main/java/io/spring/workshop/circuitdb/CircuitDbApplication.java
@@ -2,8 +2,10 @@ package io.spring.workshop.circuitdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class CircuitDbApplication {
 
 	public static void main(String[] args) {
