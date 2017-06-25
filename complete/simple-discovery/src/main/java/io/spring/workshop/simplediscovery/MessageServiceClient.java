5b58aea6a76c8c674150dc406e46331144c668e4
aripka <aripka@10.80.4.138>
2017-06-25 13:49:13 -0500

    add in circuit breaking

--- complete/simple-discovery/src/main/java/io/spring/workshop/simplediscovery/MessageServiceClient.java
+++ complete/simple-discovery/src/main/java/io/spring/workshop/simplediscovery/MessageServiceClient.java
@@ -8,12 +8,16 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @Service
 public class MessageServiceClient {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
-
+	
+	@HystrixCommand(commandKey="remoteMessage", fallbackMethod="defaultMessage", commandProperties={@HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="4")})
 	public ClientMessage getMessage() {
 
 		List<ServiceInstance> list = discoveryClient.getInstances("messenger");
@@ -26,6 +30,11 @@ public class MessageServiceClient {
 
 		return new ClientMessage("simple-discovery", remoteMessage);
 
+	}
+	
+	
+	public ClientMessage defaultMessage(){
+		return new ClientMessage("simple-discovery", "remote message offline");
 	}
 
 	
