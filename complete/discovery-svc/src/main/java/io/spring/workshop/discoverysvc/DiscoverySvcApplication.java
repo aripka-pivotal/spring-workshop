3e75c1569fbd6a12073d6db1ba6ef9b5be3fe191
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-06-25 12:49:47 -0500

    discovery-svc start

---
+++ complete/discovery-svc/src/main/java/io/spring/workshop/discoverysvc/DiscoverySvcApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.discoverysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverySvcApplication.class, args);
+	}
+}
