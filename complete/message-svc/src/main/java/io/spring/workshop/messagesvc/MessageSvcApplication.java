4c58c9160b5fc753e4c892e40ede0c9bba5c1116
Andrew Ripka <aripka@Andrews-MBP-2.roc.mn.charter.com>
2017-06-24 21:34:18 -0500

    message-svc start

---
+++ complete/message-svc/src/main/java/io/spring/workshop/messagesvc/MessageSvcApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.messagesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSvcApplication.class, args);
+	}
+}
