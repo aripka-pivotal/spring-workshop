2a2556f973b9e499a73300df1ddcf185880f3510
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-06-25 11:09:21 -0500

    client-lb start

---
+++ complete/client-lb/src/main/java/io/spring/workshop/clientlb/ClientLbApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.clientlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientLbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientLbApplication.class, args);
+	}
+}
